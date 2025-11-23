2a8b8a0db27fd627d1e298d5b7cb6e1c7519d7a4
morarafrank <morarafrank11@gmail.com>
2025-11-23 11:45:58 +0300

    set up dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,25 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.compose.plugin)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.kotlinParcelize)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
     namespace = "com.morarafrank.rickandmorty"
-    compileSdk {
-        version = release(36)
-    }
+//    compileSdk {
+//        version = release(36)
+//    }
 
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.morarafrank.rickandmorty"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -42,19 +48,95 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Navigation Compose
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
+    // Coroutines
+    implementation (libs.kotlinx.coroutines.core)
+    implementation (libs.kotlinx.coroutines.android)
+    implementation (libs.kotlinx.coroutines.play.services)
+
+    // Hilt
+    implementation (libs.hilt.android)
+//    kapt(libs.hilt.android.compiler)
+//    kapt(libs.androidx.hilt.compiler)
+    ksp (libs.hilt.android.compiler)
+    ksp (libs.androidx.hilt.compiler)
+    implementation (libs.androidx.hilt.navigation.compose)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit2.converter.gson)
+
+    // OkHttp
+    implementation (libs.okhttp)
+    implementation (libs.logging.interceptor)
+    implementation(libs.retrofit2.kotlin.coroutines.adapter)
+
+    // JSON Parsing
+    implementation (libs.gson)
+    implementation (libs.converter.gson)
+
+    // Kotlin Serialization
+    implementation(libs.kotlinx.serialization.json)
+
+    //    Moshi
+    implementation (libs.squareup.moshi.kotlin)
+    implementation (libs.converter.moshi)
+
+    // Coil
+    implementation (libs.coil.compose)
+
+    // Shared Preferences
+//    implementation (libs.androidx.preference.ktx)
+    implementation(libs.androidx.security.crypto)
+
+
+    // Room
+    implementation (libs.androidx.room.runtime)
+    implementation (libs.androidx.room.ktx)
+    ksp (libs.room.compiler)
+    annotationProcessor (libs.room.compiler)
+
+    // ViewModel
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    // ViewModel utilities for Compose
+    implementation (libs.androidx.lifecycle.viewmodel.compose)
+    // LiveData
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+
+    // Saved state module for ViewModel
+    implementation (libs.androidx.lifecycle.viewmodel.savedstate)
+
+    implementation(libs.androidx.lifecycle.runtime.ktx.v262)
+
+
+    // Annotation processor
+    ksp (libs.androidx.lifecycle.compiler)
+
+    // Lottie
+    implementation(libs.lottie.compose)
+
+    // Coil compose
+    implementation(libs.coil.compose)
+
+
 }
